8fc990d2f503e0a7c1dd5ad01fb7d67c8c58963c
tripleawwy <bjoern.marten@hotmail.de>
2020-08-18 11:22:44 +0200

    Add RegionNames to avoid using magic strings

---
+++ PrismBotV2.Core/RegionNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrismBotV2.Core
+{
+    public static class RegionNames
+    {
+        public static string ContentRegion = "ContentRegion";
+        public static string MenuRegion = "MenuRegion";
+        public static string StatusRegion = "StatusRegion";
+        public static string NavigationRegion = "NavigationRegion";
+    }
+}
