70e8ec139512f348c97f9cd9531657e2d984670d
tripleawwy <bjoern.marten@hotmail.de>
2020-08-21 13:07:19 +0200

    Add MemoryHelper

---
+++ PrismBotV2.Core/MemoryHelper/MemoryReader.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+using static PrismBotV2.Core.DLLImports.Kernel32DLL;
+
+namespace PrismBotV2.Core.MemoryHelper
+{
+    /// <summary>
+    /// A helperclass that allows the user to read chunks of memory
+    /// </summary>
+    public static class MemoryReader
+    {
+        /// <summary>
+        /// Returns an int value from an external program given at a certain address in memory
+        /// </summary>
+        /// <param name="processHandle">Handle to an external programm</param>
+        /// <param name="targetAddress">Expects an accessible memory address</param>
+        /// <returns></returns>
+        public static int ReadInt32(IntPtr processHandle, IntPtr targetAddress)
+        {
+            byte[] buffer = new byte[sizeof(int)];
+            if (ReadProcessMemory(processHandle, targetAddress, buffer, (uint)buffer.Length, out _))
+            {
+                return BitConverter.ToInt32(buffer, 0);
+            }
+            return 0;
+        }
+
+        internal static bool ReadBoolean(IntPtr processHandle, IntPtr targetAddress)
+        {
+            byte[] buffer = new byte[sizeof(bool)];
+            if (ReadProcessMemory(processHandle, targetAddress, buffer, (uint)buffer.Length, out _))
+            {
+                return BitConverter.ToBoolean(buffer, 0);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns an unsigned int value from an external program given at a certain address in memory
+        /// </summary>
+        /// <param name="processHandle">Handle to an external programm</param>
+        /// <param name="targetAddress">Expects an accessible memory address</param>
+        /// <returns></returns>
+        public static uint ReadUInt32(IntPtr processHandle, IntPtr targetAddress)
+        {
+            byte[] buffer = new byte[sizeof(int)];
+            ReadProcessMemory(processHandle, targetAddress, buffer, (uint)buffer.Length, out _);
+            return BitConverter.ToUInt32(buffer, 0);
+        }
+
+        /// <summary>
+        /// Returns a float value from an external program given at a certain address in memory
+        /// </summary>
+        /// <param name="processHandle">Handle to an external programm</param>
+        /// <param name="targetAddress">Expects an accessible memory address</param>
+        /// <returns></returns>
+        public static float ReadFloat(IntPtr processHandle, IntPtr targetAddress)
+        {
+            byte[] buffer = new byte[sizeof(float)];
+            ReadProcessMemory(processHandle, targetAddress, buffer, (uint)buffer.Length, out _);
+            return BitConverter.ToSingle(buffer, 0);
+        }
+
+        public static IntPtr ReadIntPtr(IntPtr processHandle, IntPtr targetAddress)
+        {
+            byte[] buffer = new byte[sizeof(float)];
+            ReadProcessMemory(processHandle, targetAddress, buffer, (uint)buffer.Length, out _);
+            return new IntPtr(BitConverter.ToInt32(buffer, 0));
+        }
+
+        /// <summary>
+        /// Returns a string from an external program given at a certain address in memory
+        /// </summary>
+        /// <param name="processHandle">Handle to an external programm</param>
+        /// <param name="targetAddress">Expects an accessible memory address</param>
+        /// <param name="stringLength">Specifies the length of the string to be returned</param>
+        /// <returns></returns>
+        public static string ReadString(IntPtr processHandle, IntPtr targetAddress, int stringLength)
+        {
+            byte[] buffer = new byte[stringLength];
+            ReadProcessMemory(processHandle, targetAddress, buffer, (uint)buffer.Length, out _);
+            return Encoding.ASCII.GetString(buffer);
+        }
+
+        /// <summary>
+        /// Returns a byte array from an external program given at a certain address in memory
+        /// </summary>
+        /// <param name="processHandle">Handle to an external programm</param>
+        /// <param name="targetAddress">Expects an accessible memory address</param>
+        /// <param name="byteArrayLength">Specifies the length of the string to be returned</param>
+        /// <returns></returns>
+        public static byte[] ReadByteArray(IntPtr processHandle, IntPtr targetAddress, int byteArrayLength)
+        {
+            byte[] buffer = new byte[byteArrayLength];
+            ReadProcessMemory(processHandle, targetAddress, buffer, (uint)buffer.Length, out _);
+            return buffer;
+        }
+    }
+}
---
+++ PrismBotV2.Core/MemoryHelper/MemoryWriter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text;
+using static PrismBotV2.Core.DLLImports.Kernel32DLL;
+
+namespace PrismBotV2.Core.MemoryHelper
+{
+    /// <summary>
+    /// A helperclass that allows the user to modify external program's memory space
+    /// </summary>
+
+    public static class MemoryWriter
+    {
+        /// <summary>
+        /// Writes a float value to an external program at a certain address in memory
+        /// </summary>
+        /// <param name="processHandle">Handle to an external programm</param>
+        /// <param name="targetAddress">Expects an accessible memory address</param>
+        /// <param name="value">No further explanation needed</param>
+        public static void WriteValue(IntPtr processHandle, IntPtr targetAddress, float value)
+        {
+            byte[] buffer = BitConverter.GetBytes(value);
+            WriteProcessMemory(processHandle, targetAddress, buffer, buffer.Length, out _);
+        }
+
+        /// <summary>
+        /// Writes an int value to an external program at a certain address in memory
+        /// </summary>
+        /// <param name="processHandle">Handle to an external programm</param>
+        /// <param name="targetAddress">Expects an accessible memory address</param>
+        /// <param name="value">No further explanation needed</param>
+        public static void WriteValue(IntPtr processHandle, IntPtr targetAddress, int value)
+        {
+            byte[] buffer = BitConverter.GetBytes(value);
+            WriteProcessMemory(processHandle, targetAddress, buffer, sizeof(int), out _);
+        }
+
+        /// <summary>
+        /// Writes a bool value to an external program at a certain address in memory
+        /// </summary>
+        /// <param name="processHandle">Handle to an external programm</param>
+        /// <param name="targetAddress">Expects an accessible memory address</param>
+        /// <param name="value">No further explanation needed</param>
+        public static void WriteValue(IntPtr processHandle, IntPtr targetAddress, bool value)
+        {
+            byte[] buffer = BitConverter.GetBytes(value);
+            WriteProcessMemory(processHandle, targetAddress, buffer, sizeof(bool), out _);
+        }
+
+        /// <summary>
+        /// Writes a char value to an external program at a certain address in memory
+        /// </summary>
+        /// <param name="processHandle">Handle to an external programm</param>
+        /// <param name="targetAddress">Expects an accessible memory address</param>
+        /// <param name="value">No further explanation needed</param>
+        public static void WriteValue(IntPtr processHandle, IntPtr targetAddress, char value)
+        {
+            byte[] buffer = BitConverter.GetBytes(value);
+            WriteProcessMemory(processHandle, targetAddress, buffer, sizeof(char), out _);
+        }
+
+        /// <summary>
+        /// Writes a string value to an external program at a certain address in memory
+        /// </summary>
+        /// <param name="processHandle">Handle to an external programm</param>
+        /// <param name="targetAddress">Expects an accessible memory address</param>
+        /// <param name="bunchOfChars">No further explanation needed</param>
+        public static void WriteString(IntPtr processHandle, IntPtr targetAddress, string bunchOfChars)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(bunchOfChars);
+            WriteProcessMemory(processHandle, targetAddress, buffer, bunchOfChars.Length, out _);
+        }
+    }
+}
